3b0179529791954cd0b3abb802f2990d9bb58972
taylorafox1 <taylorafox1@gmail.com>
2018-11-25 12:46:47 -0500

    moved App folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import { Button } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
       <div >
         <h1>Re-vents</h1>
+        
       </div>
     );
   }
