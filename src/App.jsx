77886b9fcc314b8e633fa655bfae72c39d4fadc9
taylorafox1 <taylorafox1@gmail.com>
2018-11-25 13:01:34 -0500

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import EventDashboard from './event/EventDashboard';
+import NavBar from './nav/NavBar';
 
 class App extends Component {
   render() {
     return (
-      <div >
+      <div > 
+        <NavBar />
         <h1>Re-vents</h1>
-        
+        <EventDashboard />
+       
       </div>
     );
   }
