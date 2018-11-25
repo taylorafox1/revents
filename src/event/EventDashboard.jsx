1319100ef245b222ff87f892a8976ff19281827c
taylorafox1 <taylorafox1@gmail.com>
2018-11-25 14:54:14 -0500

    add event handlers

--- src/event/EventDashboard.jsx
+++ src/event/EventDashboard.jsx
@@ -59,11 +59,13 @@ const eventsDashboard = [
 class EventDashboard extends Component {
   state = {
       events: eventsDashboard,
-      isOpen: false
+      isOpen: false,
+      selectedEvent: null
     }
 
   handleFormOpen = () => {
     this.setState({
+      selectedEvent: null,
       isOpen: true
     })
   }
@@ -71,6 +73,13 @@ class EventDashboard extends Component {
   handleCancel = () => {
     this.setState({
       isOpen: false
+    })
+  }
+
+  handleEditEvent = eventToUpdate => () => {
+    this.setState({
+      selectedEvent: eventToUpdate,
+      isOpen: true
     })
   }
 
@@ -85,15 +94,16 @@ class EventDashboard extends Component {
   }
 
   render() {
+    const {selectedEvent} = this.state;
     return (
       <Grid>
         <Grid.Column width={8}>
-          <EventList events={this.state.events}/>
+          <EventList onEventEdit={this.handleEditEvent} events={this.state.events}/>
         </Grid.Column>
         <Grid.Column width={8}>
           <Button onClick={this.handleFormOpen} positive content='Create Event' />
           {this.state.isOpen &&
-          <EventForm createEvent={this.handleCreateEvent} handleCancel={this.handleCancel}/>}
+          <EventForm selectedEvent={selectedEvent} createEvent={this.handleCreateEvent} handleCancel={this.handleCancel}/>}
         </Grid.Column>
       </Grid>
     )
